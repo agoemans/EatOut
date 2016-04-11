21f6979473961c579e07e6bbfc7346df1083cd19
QKwallen <kewlboontjies@gmail.com>
2016-04-11 21:59:26 +0200

    still tweaking map

--- views/js/map.js
+++ views/js/map.js
@@ -18,29 +18,37 @@ function updateMap() {
 
     dataProcessor.fetchData();
 
-
-
 }
 
 function gotData(data) {
-
+    var markerPositionList = [];
     var res = cleanAPIData.getStreetNames(data);
 
-    for(var i=0; i < res.length; res++){
-        var markerPos = new google.maps.LatLng(res[i].lat, res[i].lng);
+    for(var i=0; i < res.length; i++){
+
+        var markerPos = {lat: parseFloat(res[i].lat), lng: parseFloat(res[i].lng)};
         var marker = new google.maps.Marker({
             position: markerPos,
             map: map,
             title: 'Hello World!'
         });
-        marker.setMap(map);
+        markerPositionList.push(marker);
+    }
+    //return markerPositionList;
+    setMarkerOnMap(markerPositionList);
+}
+
+
+function setMarkerOnMap(data){
+    var markerGroup = data;
+
+    for(var i=0; i < markerGroup.length; i++){
+        var location = markerGroup[i];
+        window.setTimeout(function(myMap){
+            return function (){ myMap.setMap(map);}
+        } (location), i * 200);
     }
 
-    //var marker = new google.maps.Marker({
-    //    position: myLatLng,
-    //    map: map,
-    //    title: 'Hello World!'
-    //});
 }
 
 updateMap();
