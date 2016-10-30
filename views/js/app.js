a972c451dfcadd2aee3b04732a7e81b6276fc020
agoemans <kewlboontjies@gmail.com>
2016-10-30 16:27:07 +0100

    precleanup

--- views/js/app.js
+++ views/js/app.js
@@ -1,15 +1,41 @@
 var app = (function(){
-	var xhttp;
-	var url = apiURL;
+	function initMap() {
+		map = new google.maps.Map(document.getElementById('map-canvas'), {
+			center: {lat: 52.3837955, lng: 4.9130078},
+			zoom: 13,
+			mapTypeControlOptions: {
+				mapTypeIds: ['roadmap', 'styled_map']
+			},
+			mapTypeIds: 'styled_map'
+		});
+
+		createStyledMap();
+
+		map.mapTypes.set('styled_map', styledMapType);
+		map.setMapTypeId('styled_map');
+
+		google.maps.event.addListener(map, 'idle', showMarkers);
+	}
+
+	function createStyledMap() {
+		styledMapType = new google.maps.StyledMapType(
+				styledMapTypeData.styleElement,
+				{name: 'styled_map'});
+	}
+
+	function showMarkers() {
+		//todo use this for cluster bounds fix
+		var testLatLng = {lat: 52.378010, lng: 4.895868};
+		var bounds = map.getBounds();
+		var ne = bounds.getNorthEast();
+		var sw = bounds.getSouthWest();
+		console.log(ne.lat(), ne.lng(), sw.lat(), sw.lng())
+	}
 
 	return {
 		updateMap: function (){
-			//todo rename the ajax class once code is refactored
 			initMap();
-			//dataProcessor.fetchData();
-			//dataProcessor = new dataProcessor(this.gotData, this);
-			ajaxHelper.callbackHandler(this.gotData, this);
-			ajaxHelper.fetchData(this.gotData, this);
+			mapHandler.createMarkerList();
 		}
 	}
 
@@ -120,4 +146,4 @@ function showMarkers() {
 	console.log(ne.lat(), ne.lng(), sw.lat(), sw.lng())
 }
 
-updateMap();
+app.updateMap();
